80e12150081a39ede06136f962585653d9c5162e
Alessandro Parrilla <paggio96@gmail.com>
2022-07-15 19:20:17 +0200

    Saving a new post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
 
 function App() {
   return (
    <main className="App">
+    <AddPostForm />
     <PostsList />
    </main>
   );

4de7380f0bdb10dcfa96292b6adb2f7b634d35d5
Alessandro Parrilla <paggio96@gmail.com>
2022-07-16 10:01:56 +0200

    Import PostAutor into PostsList, now author of post is visible in the post article

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,6 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postsSlice";
-
+import { useSelector } from 'react-redux'
+import { selectAllPosts } from './postsSlice'
+import PostAuthor from './PostAuthor'
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts)
@@ -8,7 +8,10 @@ const PostsList = () => {
   const renderPosts = posts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
-      <p>{post.content.substring(0,100)}</p>
+      <p>{post.content.substring(0, 100)}</p>
+      <p className='postCredit'>
+        <PostAuthor userId={post.userId} />
+      </p>
     </article>
   ))
 
